eb39c0e6edb0242d2dcc0428219ab70d22700266
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2023-12-16 23:59:07 +0100

    vowel counter finished

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
+const word = document.querySelector(".input-text");
 const btn = document.querySelector(".btn");
 const result = document.querySelector(".message");
 
-btn.addEventListener("click", palindrome);
+btn.addEventListener("click", vowelCount);
 
-function palindrome() {
-  const word = document.querySelector(".input-text").value;
-  let len = word.length;
-  let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
-  let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
-
-  let flip = end.split("").reverse().join("");
-  if (start == flip) {
-    result.innerHTML = `${word.toUpperCase()} is a Palindrome`;
-  } else {
-    result.innerHTML = `${word.toUpperCase()} is not a Palindrome`;
+function vowelCount() {
+  let vowelCountNum = 0;
+  let wordVal = word.value.toLowerCase();
+  for (let i = 0; i < wordVal.length; i++) {
+    let letter = wordVal[i];
+    if (letter.match(/([a,e,i,o,u])/)) {
+      vowelCountNum++;
+    }
   }
+  result.innerHTML = `${word.value.toUpperCase()} has ${vowelCountNum} vowels`;
 }
